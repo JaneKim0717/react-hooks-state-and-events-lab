9109f0328ea7725c4d9b4ec91e28d0f0bf9bb284
Jane Kim <jane.kim0717@gmail.com>
2022-02-27 21:59:45 -0500

    commit tree 4b559cb07067068d22b627b41d1f4f5570b59b09

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
+  const [toggle, setToggle] = useState(false)
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  const appClass = toggle ? "App dark" : "App light"
+
+  function handleDarkModeClick() {
+    setToggle((toggle) => !toggle)
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleDarkModeClick}>{toggle ? "Dark" : "Light"}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import ShoppingList from "./ShoppingList";
+import itemData from "../data/items";
 
-function Item({ name, category }) {
+function App() {
+  const [items, setItems] = useState(itemData);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  const appClass = isDarkMode ? "App dark" : "App light";
+
+  // replace 'false' with a state variable that can be toggled between true and false
+  // this will be used for the Dark Mode Toggle feature
+
   return (
-    <li className="">
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
-    </li>
+    <div className={appClass}>
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={handleDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+      <ShoppingList items={items} />
+    </div>
   );
 }
 
-export default Item;
+export default App;
